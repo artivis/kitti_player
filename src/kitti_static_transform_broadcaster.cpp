4746655646d59ffb2144b80f5fa91f2dc4391b26
Ellon Mendes <ellon.mendes@laas.fr>
2016-04-07 13:15:58 +0200

    Improve kitti_static_transform_broadcaster to accept cam_to_cam calibration files

--- src/kitti_static_transform_broadcaster.cpp
+++ src/kitti_static_transform_broadcaster.cpp
@@ -34,13 +34,112 @@
 
 #include <tf2/LinearMath/Quaternion.h>
 #include <tf2/LinearMath/Matrix3x3.h> 
-#include "tf2_ros/static_transform_broadcaster.h"
+#include <tf2/LinearMath/Transform.h> 
+#include <tf2/convert.h>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
+
+#include <tf2_ros/static_transform_broadcaster.h>
 
 #include <boost/algorithm/string.hpp>
 #include <boost/lexical_cast.hpp>
 
 using namespace std;
 using namespace boost;
+
+/// Fill up translation part of msg
+void parse_translation(string value_string, geometry_msgs::TransformStamped & msg)
+{
+  typedef vector< string > split_vector_type;
+  split_vector_type SplitVec;
+
+  trim(value_string);
+  split( SplitVec, value_string, is_any_of(" "), token_compress_on );         
+
+  msg.transform.translation.x = lexical_cast<double>(SplitVec[0]);
+  msg.transform.translation.y = lexical_cast<double>(SplitVec[1]);
+  msg.transform.translation.z = lexical_cast<double>(SplitVec[2]);
+}
+
+/// Fill up rotation part of msg
+void parse_rotation(string value_string, geometry_msgs::TransformStamped & msg)
+{
+  typedef vector< string > split_vector_type;
+  split_vector_type SplitVec;
+
+  trim(value_string);
+  split( SplitVec, value_string, is_any_of(" "), token_compress_on );
+
+  tf2::Matrix3x3 mat(lexical_cast<double>(SplitVec[0]),lexical_cast<double>(SplitVec[1]),lexical_cast<double>(SplitVec[2]),
+                   lexical_cast<double>(SplitVec[3]),lexical_cast<double>(SplitVec[4]),lexical_cast<double>(SplitVec[5]),
+                   lexical_cast<double>(SplitVec[6]),lexical_cast<double>(SplitVec[7]),lexical_cast<double>(SplitVec[8]));
+  tf2::Quaternion quat;
+  mat.getRotation(quat);
+  msg.transform.rotation.x = quat.x();
+  msg.transform.rotation.y = quat.y();
+  msg.transform.rotation.z = quat.z();
+  msg.transform.rotation.w = quat.w();
+}
+
+bool cam_calib_file_to_tf_stamped(string filename, vector<geometry_msgs::TransformStamped> & msgs)
+{
+  bool found_T_01 = false, found_R_01 = false;
+  bool found_T_02 = false, found_R_02 = false;
+  bool found_T_03 = false, found_R_03 = false;
+
+  ifstream file(filename);
+  string line;
+  if (file.is_open())
+  {
+    while ( getline (file,line) )
+    {
+      // Split the line into :'s 
+      typedef vector< string > split_vector_type;
+      split_vector_type SplitVec;
+      split( SplitVec, line, is_any_of(":"), token_compress_on );
+
+      if(!SplitVec.empty() && SplitVec[0] == "T_01")
+      {
+        parse_translation(SplitVec[1],msgs[0]);
+        found_T_01 = true;
+      }
+      else if(!SplitVec.empty() && SplitVec[0] == "R_01")
+      {
+        parse_rotation(SplitVec[1],msgs[0]);
+        found_R_01 = true;
+      }
+      else if(!SplitVec.empty() && SplitVec[0] == "T_02")
+      {
+        parse_translation(SplitVec[1],msgs[1]);
+        found_T_02 = true;
+      }
+      else if(!SplitVec.empty() && SplitVec[0] == "R_02")
+      {
+        parse_rotation(SplitVec[1],msgs[1]);
+        found_R_02 = true;
+      }
+      else if(!SplitVec.empty() && SplitVec[0] == "T_03")
+      {
+        parse_translation(SplitVec[1],msgs[2]);
+        found_T_03 = true;
+      }
+      else if(!SplitVec.empty() && SplitVec[0] == "R_03")
+      {
+        parse_rotation(SplitVec[1],msgs[2]);
+        found_R_03 = true;
+      }
+    }
+    file.close();
+  }
+
+  // Return success if we found all T and R in the file
+  if(found_T_01 && found_R_01 &&
+     found_T_02 && found_R_02 &&
+     found_T_03 && found_R_03)
+      return false;
+
+  // Error
+  return true;
+}
 
 /// Reads a calibration file and fills the TransformedStamped with the transformation
 bool calib_file_to_tf_stamped(string filename, geometry_msgs::TransformStamped & msg)
@@ -60,30 +159,12 @@ bool calib_file_to_tf_stamped(string filename, geometry_msgs::TransformStamped &
 
       if(!SplitVec.empty() && SplitVec[0] == "T")
       {
-        // Fill up translation part
-        string value_string = trim_copy(SplitVec[1]);
-        SplitVec.clear();
-        split( SplitVec, value_string, is_any_of(" "), token_compress_on );         
-        msg.transform.translation.x = lexical_cast<double>(SplitVec[0]);
-        msg.transform.translation.y = lexical_cast<double>(SplitVec[1]);
-        msg.transform.translation.z = lexical_cast<double>(SplitVec[2]);
+        parse_translation(SplitVec[1],msg);
         found_T = true;
       }
       else if(!SplitVec.empty() && SplitVec[0] == "R")
       {
-        // Fill up rotation part
-        string value_string = trim_copy(SplitVec[1]);
-        SplitVec.clear();
-        split( SplitVec, value_string, is_any_of(" "), token_compress_on );
-        tf2::Matrix3x3 mat(lexical_cast<double>(SplitVec[0]),lexical_cast<double>(SplitVec[1]),lexical_cast<double>(SplitVec[2]),
-                           lexical_cast<double>(SplitVec[3]),lexical_cast<double>(SplitVec[4]),lexical_cast<double>(SplitVec[5]),
-                           lexical_cast<double>(SplitVec[6]),lexical_cast<double>(SplitVec[7]),lexical_cast<double>(SplitVec[8]));
-        tf2::Quaternion quat;
-        mat.getRotation(quat);
-        msg.transform.rotation.x = quat.x();
-        msg.transform.rotation.y = quat.y();
-        msg.transform.rotation.z = quat.z();
-        msg.transform.rotation.w = quat.w();
+        parse_rotation(SplitVec[1],msg);
         found_R = true;
       }
     }
@@ -115,22 +196,63 @@ int main(int argc, char ** argv)
     }
 
     msg.header.stamp = ros::Time::now();
-    msg.header.frame_id = argv[2];
-    msg.child_frame_id = argv[3];
+    msg.child_frame_id = argv[2];
+    msg.header.frame_id = argv[3];
 
     broadcaster.sendTransform(msg);
-    ROS_INFO("Spinning until killed publishing %s to %s", msg.header.frame_id.c_str(), msg.child_frame_id.c_str());
+    ROS_INFO("Spinning until killed publishing %s to %s", msg.child_frame_id.c_str(), msg.header.frame_id.c_str());
     ros::spin();
 
     return 0;
+  }
+  else if(argc == 6)
+  {
+    vector<geometry_msgs::TransformStamped> msgs(3);
+
+    if(cam_calib_file_to_tf_stamped(argv[1],msgs))
+    {
+      ROS_ERROR("error parsing calib file");
+      return -1;
+    }
+
+    msgs[0].child_frame_id = argv[3];
+    msgs[1].child_frame_id = argv[4];
+    msgs[2].child_frame_id = argv[5];
+
+    std::for_each(msgs.begin(),msgs.end(),[&] (geometry_msgs::TransformStamped & msg) 
+    {
+      // Invert transform messages. This is necessary because the cam
+      // transforms defines image_00 in the frame of image_{01,02,03}, and
+      // this is not suitable for TF since it means image_00 will have
+      // multiple parents in the tf tree. Inverting makes image_{01,02,03}
+      // childs of image_00
+      tf2::Transform tf;
+      tf2::convert(msg.transform,tf);
+      tf2::convert(tf.inverse(),msg.transform);
+
+      // Fill up header
+      msg.header.frame_id = argv[2];
+      msg.header.stamp = ros::Time::now();
+
+      broadcaster.sendTransform(msg);
+    } );
+    
+    ROS_INFO("Spinning until killed. Publishing: %s to %s; %s to %s; and %s to %s",
+      msgs[0].child_frame_id.c_str(), msgs[0].header.frame_id.c_str(),
+      msgs[1].child_frame_id.c_str(), msgs[1].header.frame_id.c_str(),
+      msgs[2].child_frame_id.c_str(), msgs[2].header.frame_id.c_str());
+    ros::spin();
+
+    return 0;    
   }
   else
   {
     printf("A command line utility for manually sending a transform.\n");
     //printf("It will periodicaly republish the given transform. \n");
-    printf("Usage: kitti_static_transform_publisher calib_file.txt frame_id child_frame_id \n");
-    printf("\nThis transform is the transform of the coordinate frame from frame_id into the coordinate frame \n");
-    printf("of the child_frame_id.  \n");
+    printf("Usage: kitti_static_transform_publisher calib_X_to_Y.txt frame_id_X frame_id_Y \n");
+    printf("OR \n");
+    printf("Usage: kitti_static_transform_publisher calib_cam_to_cam.txt frame_id_image_00 frame_id_image_01 frame_id_image_02 frame_id_image_03 \n");
+    printf("\nThis transform is the transform of the coordinate frame child_frame_id into the coordinate frame frame_id.\n");
     ROS_ERROR("kitti_static_transform_publisher exited due to not having the right number of arguments");
     return -1;
   }
